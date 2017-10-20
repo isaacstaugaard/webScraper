c1cca640356cedc914aa9ddb054674a5775892a8
Isaac Staugaard <isaacestaugaard@gmail.com>
2017-10-20 15:14:15 -0700

    Now creates a table called flightdata filled with the flight information in PostgreSQL
    (Doesn't work on some dates for some reason)

--- skiplaggedScraper.py
+++ skiplaggedScraper.py
@@ -9,12 +9,28 @@ from selenium.webdriver.support.ui import WebDriverWait			  #Allows for the driv
 from selenium.webdriver.common.keys import Keys                   #Allows for scrolling down (PGDOWN key)
 from selenium.webdriver.common.action_chains import ActionChains  #Allows for mouseOver
 import smtplib													  #Allows use of email
- 
+import psycopg2
+
+conn = psycopg2.connect(database="DATABASE", user = "USER", password = "PASSWORD", host = "127.0.0.1", port = "PORT")
+print ("PostgreSQL: Opened database successfully")
+cur = conn.cursor()
+
+# ------------------ This is used to create a new table, only needs to be done once ----------------------- #
+#cur.execute('''CREATE TABLE FLIGHTDATA
+#      (DURATION text,
+#      STOPS text, 
+#      deptTime text, 
+#      deptAir text, 
+#      arrTime text, 
+#      arrAir text, 
+#      price text);''')
+#print ("PostgreSQL: Table created successfully")
+#conn.commit() 
 
 def scrapeWebsite(source,destination,date):
 	driver = webdriver.Chrome()
 	driver.get("https://skiplagged.com/flights/{0}/{1}/{2}".format(source,destination,date))  #Opens the driver to the correct webpage
-	time.sleep(1)  #Let the webpage load before maximizing
+	#time.sleep(1)  #Let the webpage load before maximizing
 	driver.maximize_window() #For maximizing window
 	time.sleep(1) #Let the webpage sit until we move the mouse
 
@@ -24,7 +40,7 @@ def scrapeWebsite(source,destination,date):
 	driver.find_element_by_xpath(xpathToCurrencyDropdown).click()
 	time.sleep(.5)
 	driver.find_element_by_xpath(xpathToUSDOption).click()
-	time.sleep(2)
+	time.sleep(8) #Let the webpage load fully (takes a while for the spirit airlines to show up)
 
 	# -- Move mouse to hover over the first element on the PAGE_DOWN -- #
 	xpathToTripListSections = '//*[@id="trip-list-sections"]/div[2]'
@@ -32,13 +48,6 @@ def scrapeWebsite(source,destination,date):
 	hov = ActionChains(driver).move_to_element(element)
 	hov.perform()
 	time.sleep(1)
-
-	# -------- Try to print out all the different attributes from JUST the first element -------- #
-	# pathToFirstElemLength = '//*[@id="4d7ded7b19fd4946b25526c94ef76a3634ed8fac852a28bece2368b73ee2800fcd5701ec4aa59898b872713bc8ffe353ecd233c745b206d0e154bc154f4e87f7b50867c321d50a993424cf6f04aca609f6525edcf53a19260513ef4810a586f9"]/div[1]'
-	# first =  driver.find_elements_by_xpath(pathToFirstElemLength)
-	# hello = first[0].text.split('\n')
-	# print(first[0].text)
-	# print(hello)
 
 	duration = []
 	stops = []
@@ -73,27 +82,33 @@ def scrapeWebsite(source,destination,date):
 				if (i%7 == 6):
 					price.append(hello[i])																							
 				i = i+1
-			# -------- Prints all of the elements one by one, one per line -------- #
-			#print (elem.text)
 
 		elem1.send_keys(Keys.PAGE_DOWN)
-		time.sleep(1)  #Time in between scrolls
+		time.sleep(.5)  #Time in between scrolls
 		no_of_pagedowns-=1
 	
-	# -------- These are print functions for each indiviual value -------- #
-	#print("Duration: ", duration, "\n")
-	#print("Stops: ", stops, "\n")
-	#print("Departure Time: ", deptTime, "\n")
-	#print("Departure Airport: ", deptAir, "\n")
-	#print("Arrival Time: ", arrAir, "\n")
-	#print("Arrival Airport: ", arrAir, "\n")
-	#print("Price: ", price, "\n")
 	k=0
 	numElements = len(duration)
 	while (k < numElements):
 		flights.append( [ duration[k], stops[k], deptTime[k], deptAir[k], arrTime[k], arrAir[k], price[k] ] )
 		k += 1
-	print("Flights: ", flights)
+	print("Flights: ", flights, "\n")
+
+	#------------------- DELETE THE DATA CURRENTLY STORED IN FLIGHTDATA ------------------------------------------#
+	cur.execute("DELETE FROM FLIGHTDATA")
+
+
+	#------------------- THIS PUTS THE CURRENT DATA INTO THE SQL TABLE CALLED FLIGHTDATA --------------------------#
+	z=0
+	while (z < numElements):
+		cur.execute("INSERT INTO FLIGHTDATA (DURATION,STOPS,DEPTTIME,DEPTAIR,ARRTIME,ARRAIR,PRICE) \
+      		VALUES (%s, %s, %s, %s, %s, %s, %s)", (flights[z][0], flights[z][1], flights[z][2], flights[z][3], flights[z][4], flights[z][5], flights[z][6] ));
+		z += 1
+
+	conn.commit()
+	print ("PostgreSQL: Records created successfully")
+	conn.close()
+
 
 	lowestPrice = int(price[0].replace("$",""))
 	if (lowestPrice <= targetPrice):
@@ -120,6 +135,8 @@ if __name__=="__main__":
 	print ("Fetching flight details\n")
 	scraped_data = scrapeWebsite(source,destination,date)
 
+
+# ---------------------- This is for sending an email when the price is below a certain threshold --------------------- #
 	if (scraped_data == 'YES'):
 		server = smtplib.SMTP('smtp.gmail.com', 587)
 		server.starttls()
